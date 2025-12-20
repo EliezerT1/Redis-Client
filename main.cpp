d68c9a76592bf4f69d8ba3f2743fb6cb0cdbfa3e
Eliezer Then <eliezerthen@MacBook-Pro.local>
2025-12-20 04:13:43 -0500

    created main loop

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::string host = "127.0.0.1";
+    int port = 6379;
+    int i = 1;
+
+    // parse command-line args for -h and -p
+    while (i < argc){
+        std::string arg = argv[i];
+        if (arg == "-h" && i + 1 < argc) {// -h 127.0.01
+            host = argv[++i];
+        }
+        else if (arg == "-p" && i + 1 < argc) {
+            port = std::stoi(argv[++i]);
+        }
+        else{
+            break;
+        }
+        ++i;
+    }
+}
